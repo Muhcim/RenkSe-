fd66787d877d307e9abc851470baf1063de57e6b
Muhcim <34978284+Muhcim@users.noreply.github.com>
2018-02-02 23:08:10 +0300

    Renk Seçici Alpha İle

---
+++ RenkSeçici/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace RenkSeçici
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.BtnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.aval = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bval = new System.Windows.Forms.Label();
+            this.gval = new System.Windows.Forms.Label();
+            this.rval = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 212);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(367, 254);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(0, -3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(108, 81);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // BtnOpen
+            // 
+            this.BtnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnOpen.Location = new System.Drawing.Point(94, 157);
+            this.BtnOpen.Name = "BtnOpen";
+            this.BtnOpen.Size = new System.Drawing.Size(147, 49);
+            this.BtnOpen.TabIndex = 2;
+            this.BtnOpen.Text = "Resim Aç";
+            this.BtnOpen.UseVisualStyleBackColor = true;
+            this.BtnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(129, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Alpha";
+            // 
+            // aval
+            // 
+            this.aval.AutoSize = true;
+            this.aval.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.aval.Location = new System.Drawing.Point(204, 9);
+            this.aval.Name = "aval";
+            this.aval.Size = new System.Drawing.Size(20, 24);
+            this.aval.TabIndex = 4;
+            this.aval.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(144, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "R";
+            // 
+            // bval
+            // 
+            this.bval.AutoSize = true;
+            this.bval.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bval.Location = new System.Drawing.Point(204, 113);
+            this.bval.Name = "bval";
+            this.bval.Size = new System.Drawing.Size(20, 24);
+            this.bval.TabIndex = 6;
+            this.bval.Text = "0";
+            // 
+            // gval
+            // 
+            this.gval.AutoSize = true;
+            this.gval.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gval.Location = new System.Drawing.Point(204, 78);
+            this.gval.Name = "gval";
+            this.gval.Size = new System.Drawing.Size(20, 24);
+            this.gval.TabIndex = 7;
+            this.gval.Text = "0";
+            // 
+            // rval
+            // 
+            this.rval.AutoSize = true;
+            this.rval.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rval.Location = new System.Drawing.Point(204, 44);
+            this.rval.Name = "rval";
+            this.rval.Size = new System.Drawing.Size(20, 24);
+            this.rval.TabIndex = 8;
+            this.rval.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(144, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 24);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "G";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(144, 113);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 24);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "B";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 475);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.rval);
+            this.Controls.Add(this.gval);
+            this.Controls.Add(this.bval);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.aval);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnOpen);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Renk Seçici";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button BtnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label aval;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label bval;
+        private System.Windows.Forms.Label gval;
+        private System.Windows.Forms.Label rval;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ RenkSeçici/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenkSeçici
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                Color c = bmp.GetPixel(e.X, e.Y);
+                pictureBox2.BackColor = c;
+
+                aval.Text = c.A.ToString();
+                rval.Text = c.R.ToString();
+                gval.Text = c.G.ToString();
+                bval.Text = c.B.ToString();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+        }
+    }
+}
